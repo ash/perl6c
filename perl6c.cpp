22560c45ea192e3e50de82b99e5785f7e23da133
Andrew Shitov <andy@shitov.ru>
2011-06-14 07:34:48 +0200

    Parser class

--- perl6c.cpp
+++ perl6c.cpp
@@ -6,6 +6,22 @@
 #include<string>
 #include<vector>
 
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+
+namespace perl6 {
+    template<typename Iterator>
+    struct perl6_grammar: qi::grammar<Iterator, ascii::space_type>
+    {
+        perl6_grammar(): perl6_grammar::base_type(TOP)
+        {
+            TOP = ascii::char_;
+        }
+
+        qi::rule<Iterator, ascii::space_type> TOP;
+    };
+}
+
 int main(int argc, char** argv) {
     using namespace std;
 
@@ -17,7 +33,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    std::ifstream in(filename, std::ios_base::in);
+    ifstream in(filename, std::ios_base::in);
     
     if (!in) {
         cerr << "No such file: " << filename << "\n";
@@ -25,10 +41,15 @@ int main(int argc, char** argv) {
     }
 
     string source;
-    in.unsetf(ios::skipws);
+    in.unsetf(std::ios::skipws);
     copy(istream_iterator<char>(in), istream_iterator<char>(), back_inserter(source));
+
+    perl6::perl6_grammar<string::const_iterator> perl6_parser;
+    string::const_iterator iter = source.begin();
+    string::const_iterator end = source.end();
+    bool parse_result = phrase_parse(iter, end, perl6_parser, ascii::space);
     
-    cout << source;
+    cout << parse_result << endl;
     
     return 0;
 }
